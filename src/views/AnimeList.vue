36b0395f2a7b00fc412227072b7e4e42c05a53fc
asd4a10 <amanzholov000@gmail.com>
2022-08-06 16:12:14 +0600

    added router

---
+++ src/views/AnimeList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>Anime List: Here will be list of all animes</h1>
+  </div>
+</template>
+
+<script></script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>Main Page, here will be the updates, news etc</h1>
+  </div>
+</template>
+
+<script></script>
